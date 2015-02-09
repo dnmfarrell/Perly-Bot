7de1a265ee6ffd5c578277773626063330457627
sillymoose <davidnmfarrell@gmail.com>
2015-02-09 08:17:30 -0500

    organized globals, prep for config file

--- bot.pl
+++ bot.pl
@@ -12,29 +12,35 @@ use Time::Seconds;
 use Try::Tiny;
 use List::Util 'any';
 
+# Globals - these can be put in a config file
 our $VERSION      = 0.01;
-our $agent_string = "Perly_Bot/v$VERSION";
+my $agent_string = "Perly_Bot/v$VERSION";
 
 # posts must mention a Perl keyword to be considered relevant
 my $looks_perly = qr/\b(?:perl|cpan|cpanminus|moose|metacpan|modules?)\b/i;
 
 my $datetime_now = localtime;
+my $AGE_THRESHOLD= ONE_DAY;
 my $ua           = HTTP::Tiny->new( agent => $agent_string );
 my $rss          = XML::RSS::Parser->new;
 my $feeds        = LoadFile('feeds.yml');
 my $cache        = LoadFile('logs/cached_urls.yml');
-refresh_cache();
 
 open my $ERROR_LOG, '>>', 'logs/error.log' or die $!;
 
-# get reddit creds
-my $reddit_username = $ENV{REDDIT_BOT_USERNAME};
-my $reddit_password = $ENV{REDDIT_BOT_PASSWORD};
+# end globals
 
-log_error(
-    "Env vars REDDIT_BOT_USERNAME, REDDIT_BOT_PASSWORD are not both defined")
-  unless $reddit_username && $reddit_password;
+unless ($ENV{REDDIT_BOT_USERNAME}
+        && $ENV{REDDIT_BOT_PASSWORD})
+{
+    log_error(
+      "Env vars REDDIT_BOT_USERNAME, REDDIT_BOT_PASSWORD are not both defined");
+    exit 1;
+}
 
+refresh_cache(); # remove stale URLs from cache
+
+# Loop through feeds, check for new posts
 for my $feed ( @{$feeds} ) {
     next unless $feed->{active};
 
@@ -56,7 +62,7 @@ for my $feed ( @{$feeds} ) {
                     my $datetime_post =
                       Time::Piece->strptime( $datetime, $date_format );
 
-                    if ( $datetime_post > $datetime_now - ONE_DAY ) {
+                    if ( $datetime_post > $datetime_now - $AGE_THRESHOLD ) {
                         if (   $i->query('title')->text_content =~ $looks_perly
                             or $i->query('description')->text_content =~
                             $looks_perly )
@@ -73,17 +79,19 @@ for my $feed ( @{$feeds} ) {
                 };
             }
         }
-        elsif ( $feed->{type} eq 'atom' ) {
+        elsif ( $feed->{type} eq 'atom' )
+        {
             no strict 'refs';
             my $posts = XML::Atom::Feed->new( Stream => \$response->{content} );
-            foreach my $post ( $posts->entries ) {
+            foreach my $post ( $posts->entries )
+            {
                 try {
                     # extract the post date
                     my $datetime = $post->$date_name =~ s/ UTC| GMT//gr;
                     my $datetime_post =
                       Time::Piece->strptime( $datetime, $date_format );
 
-                    if ( $datetime_post > $datetime_now - ONE_DAY ) {
+                    if ( $datetime_post > $datetime_now - $AGE_THRESHOLD ) {
                         if (   $post->title =~ $looks_perly
                             or $post->summary =~ $looks_perly )
                         {
@@ -110,7 +118,8 @@ for my $feed ( @{$feeds} ) {
 
 sub log_error
 {
-    say $ERROR_LOG $datetime_now->datetime . "\t$_[0]";
+    my $timestamp = localtime;
+    say $ERROR_LOG $timestamp->datetime . "\t$_[0]";
 }
 
 sub url_is_cached
@@ -131,14 +140,15 @@ sub refresh_cache
     @$cache = grep {
         my $url_date =
           Time::Piece->strptime( $_->{datetime}, "%Y-%m-%dT%H:%M:%S" );
-        $url_date > $datetime_now - ONE_DAY ? 1 : 0;
+        $url_date > $datetime_now - $AGE_THRESHOLD ? 1 : 0;
     } @$cache;
 }
 
 sub post_reddit_link
 {
-    my ($title, $url) = @_;
+    my ($title, $url, $subreddit) = @_;
 
+    # Return if URL has already been posted
     return if url_is_cached($url);
     cache_url($url);
 
@@ -149,13 +159,13 @@ sub post_reddit_link
     );
 
     unless ( $reddit->is_logged_in ) {
-        $reddit->login( $reddit_username, $reddit_password );
+        $reddit->login( $ENV{REDDIT_BOT_USERNAME}, $ENV{REDDIT_BOT_PASSWORD} );
         $reddit->save_session();
     }
 
     try {
         $reddit->submit_link(
-            subreddit => 'perl',
+            subreddit => $subreddit,
             title     => decode_entities($title),
             url       => $url
         );
@@ -163,7 +173,7 @@ sub post_reddit_link
     catch {
         log_error("Error posting $title $url $_");
     };
-    sleep(1);
+    sleep(2); # throttle requests to avoid exceeding API limit
 }
 
 END { DumpFile( 'logs/cached_urls.yml', $cache ) }
