d8b4292c62f3b0046a94887fa2b78fc73acd5ad4
sillymoose <davidnmfarrell@gmail.com>
2015-12-23 16:10:47 +0000

    use FeedPP for RSS

--- lib/Perly/Bot/Feed.pm
+++ lib/Perly/Bot/Feed.pm
@@ -12,7 +12,6 @@ use Role::Tiny;
 use Time::Piece;
 use Time::Seconds;
 use XML::FeedPP;
-use XML::RSS::Parser;
 
 use base 'Class::Accessor';
 Perly::Bot::Feed->mk_accessors(qw/url type date_name date_format active proxy media delay_seconds twitter/);
@@ -43,7 +42,7 @@ sub new
     rss => {
     date_name   => 'pubDate',
     date_format => '%a, %d %b %Y %H:%M:%S %z',
-    parser      => 'XML::RSS::Parser',
+    parser      => 'XML::FeedPP',
       },
     atom => {
     date_name   => 'published',
@@ -88,7 +87,6 @@ sub _allowed_parsers
 {
   state $allowed = {
     map { $_ => 1 } qw(
-     XML::RSS::Parser
      XML::FeedPP
      ) };
   $allowed;
@@ -102,47 +100,24 @@ sub get_posts
 
   my @posts = ();
 
-  if ( $self->type eq 'rss' )
-  {
-      my $rss = XML::RSS::Parser->new;
-      my $parsed_xml = $rss->parse_string( $xml );
-      foreach my $i ( $parsed_xml->query('//item') )
-      {
-        # extract the post date
-        my $datetime_raw = $i->query($self->date_name)->text_content =~ s/ UTC| GMT//gr;
-        my $datetime = Time::Piece->strptime($datetime_raw, $self->date_format);
+  my $class = $self->type eq 'rss' ? 'XML::FeedPP::RSS' : 'XML::FeedPP::Atom';
 
-        push @posts, Perly::Bot::Feed::Post->new({
-          delay_seconds => $self->delay_seconds,
-          description   => $i->query('description')->text_content,
-          datetime      => $datetime,
-          title         => $i->query('title')->text_content,
-          url           => $i->query('link')->text_content,
-          proxy         => $self->proxy,
-          twitter       => $self->twitter,
-        });
-      }
-  }
-  elsif ( $self->type eq 'atom' )
+  my @items = $class->new($xml, -type => 'string')->get_item();
+  foreach my $i ( @items )
   {
-    # accessors are generated from the available tags in the atom feed
-    # the tags can have different names, so we store the tag name in feeds.yml
-    no strict 'refs';
-    my @items = XML::FeedPP::Atom->new($xml, -type => 'string')->get_item();
-    foreach my $i ( @items )
-    {
-      # extract the post date
-      my $datetime_raw = $i->get( $self->date_name ) =~ s/ UTC| GMT//gr;
-      my $datetime = Time::Piece->strptime( $datetime_raw, $self->date_format );
-
-        push @posts, Perly::Bot::Feed::Post->new({
-          description => $i->description,
-          datetime    => $datetime,
-          title       => $i->title,
-          url         => $i->link,
-          proxy       => $self->proxy,
-        });
-    }
+    # extract the post date
+    my $datetime_raw = $i->get( $self->date_name ) =~ s/ UTC| GMT//gr;
+    my $datetime = Time::Piece->strptime( $datetime_raw, $self->date_format );
+  
+      push @posts, Perly::Bot::Feed::Post->new({
+        delay_seconds => $self->delay_seconds,
+        description => $i->description,
+        datetime    => $datetime,
+        title       => $i->title,
+        url         => $i->link,
+        proxy       => $self->proxy,
+        twitter     => $self->twitter,
+      });
   }
   return \@posts
 }
