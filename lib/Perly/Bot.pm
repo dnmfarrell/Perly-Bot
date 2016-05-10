48ef38dac28a1e6bec575377d90012b22b3f54bd
David Farrell <davidnmfarrell@gmail.com>
2016-05-09 22:01:56 -0400

    version bump

--- lib/Perly/Bot.pm
+++ lib/Perly/Bot.pm
@@ -13,7 +13,7 @@ use Path::Tiny;
 use Perly::Bot::Config;
 use Data::Dumper;
 
-our $VERSION = '0.201';
+our $VERSION = '0.202';
 
 Log::Log4perl->init( \ <<'LOG');
   layout_class   = Log::Log4perl::Layout::PatternLayout
