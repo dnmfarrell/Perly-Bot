d8b4b8c384f821418d5d89eb2f5c657cb1661afb
David Farrell <davidnmfarrell@gmail.com>
2017-02-24 08:41:41 -0500

    hashtags need hashes...

--- lib/Perly/Bot/Media/Twitter.pm
+++ lib/Perly/Bot/Media/Twitter.pm
@@ -48,7 +48,7 @@ sub _build_tweet ( $self, $blog_post ) {
   my $title   = $blog_post->decoded_title;
   my $url     = $blog_post->root_url;
   my $via     = $blog_post->twitter ? 'via @' . $blog_post->twitter : '';
-  my $hashtag = $self->{hashtag};
+  my $hashtag = "#$self->{hashtag}";
 
   my $char_count = 140;
   $char_count -= $url =~ /^https/ ? 23 : 22;
